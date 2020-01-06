6a660a4c335e56fcd4c0265f3728a483c7ccf2b0
Muhamad Firhat <muhamad.firhat@gmail.com>
2020-01-06 08:36:55 +0700

    add material

---
+++ 7.2-api-routes/pages/index.js
@@ -0,0 +1,28 @@
+export default function Index() {
+  return (
+    <main className="center">
+      <div className="quote">Write tests, not too many, mostly integration</div>
+      <span className="author"> - Guillermo Rauch </span>
+
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
+        }
+      `}</style>
+    </main>
+  )
+}
