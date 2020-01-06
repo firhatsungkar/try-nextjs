b6a6589918622f528dbbbe14756b5ec5955a57a8
Muhamad Firhat <muhamad.firhat@gmail.com>
2020-01-06 08:58:49 +0700

    Navigate between pages

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,6 +1,10 @@
+import Link from 'next/link';
 export default function Index() {
   return (
     <div>
+      <Link href="/about">
+        <a title="About page">About page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   )
